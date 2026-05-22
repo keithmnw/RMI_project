cdf9ce820be55294f8ba9c20a0ef44c42dc64c1b
Keith Muleshe <muleshe13@gmail.com>
2026-05-22 11:50:48 +0300

    Added Source Files

--- src/common/StudentService.java
+++ src/common/StudentService.java
@@ -1,2 +1,18 @@
-public class StudentService {
+// Name: John Keith Muleshe  Student number: 220655
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface StudentService extends Remote {
+
+    // Q1 - Returns greeting with name
+    String greet(String name) throws RemoteException;
+
+    // Q3 - Echoes back user input with prefix
+    String echo(String userInput) throws RemoteException;
+
+    // Q4 - Returns list of students from database
+    List<String[]> getStudents() throws RemoteException;
+
 }
